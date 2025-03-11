acb9a0d50e40ecfce096c13deaee1f271f3868fa
Chiangwill129 <wchiang@chicagobooth.edu>
2025-03-11 06:50:42 +0000

    finish

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,22 @@
+class DirectorsController < ApplicationController
+  def index
+    render({:template => "director_templates/list"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({:id => the_id})
+    @the_director = matching_records.at(0)
+
+    render({:template => "director_templates/details"})
+  end
+
+  def junior
+    render({:template => "director_templates/junior"})
+  end
+
+  def senior
+    render({:template => "director_templates/senior"})
+  end
+
+end
